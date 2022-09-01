57fddfbb7f0e9c69c449c5fc93db1b93ef65ce57
aayush-galav-ext <aayush.galav.ext@biqmind.com>
2022-09-01 21:26:25 +0530

    first commit in react-cmoponent-library

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'library-react-components'
+import { ExampleComponent, Button } from 'library-react-components'
 import 'library-react-components/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <><ExampleComponent text="Create React Library Example 😄" /><Button label='yoda'/></>
 }
 
 export default App
